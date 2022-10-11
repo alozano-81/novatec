07c1d471bc590972c491eb3157e22b35b920ebde
Andres Lozano <lozano.yeison@locatelcolombia.com>
2022-10-10 23:55:31 -0500

    ajustes

--- src/main/java/com/prueba/test/Servicio/EstudianteService.java
+++ src/main/java/com/prueba/test/Servicio/EstudianteService.java
@@ -4,15 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.prueba.test.model.Estudiantes;
 import com.prueba.test.repository.EstudiantesRepository;
-import org.springframework.http.MediaType;
 
 @Service
 public class EstudianteService {
@@ -34,21 +29,25 @@ public class EstudianteService {
         return rest;
     }
 
-    @RequestMapping(value = "/crear/{id}/{nombre}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> saveUserP(@PathVariable("id") final Long id, @PathVariable("nombre") final String nombre)
-            throws Exception {
-
+    public Estudiantes crearEstudiantes(Long id, String nombre, String rol, char activo) {
         Estudiantes estcreate = new Estudiantes();
 
+        estcreate.setIdEst(id + 2);
+        estcreate.setNombre(nombre);
+        estcreate.setRol(rol);
+        estcreate.setActivo(String.valueOf(activo));
+        estudiantesRepository.saveAndFlush(estcreate);
+        return estcreate;
+    }
+
+    public Estudiantes updateEstudiantes(Long id, String nombre, String rol, char activo) {
+        Estudiantes estcreate = new Estudiantes();
         estcreate.setIdEst(id);
         estcreate.setNombre(nombre);
-        estcreate.setActivo("S");
-
-        System.out.println("Ver cretate2: " + estcreate);
-
+        estcreate.setRol(rol);
+        estcreate.setActivo(String.valueOf(activo));
         estudiantesRepository.saveAndFlush(estcreate);
-        return ResponseEntity.ok("Usuario actualizado");
-
+        return estcreate;
     }
 
 }
--- src/main/java/com/prueba/test/controller/EstudiantesController.java
+++ src/main/java/com/prueba/test/controller/EstudiantesController.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prueba.test.Servicio.EstudianteService;
 import com.prueba.test.model.Estudiantes;
-import org.springframework.http.MediaType;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.prueba.test.repository.EstudiantesRepository;
 
 @RestController
 @RequestMapping("estudiantes")
@@ -34,6 +34,33 @@ public class EstudiantesController {
         } catch (Exception e) {
             return new ResponseEntity<>(est, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @RequestMapping(value = "/crear/{id}/{nombre}/{idRol}/{activo}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> saveUserP(@PathVariable("id") final Long id, @PathVariable("nombre") final String nombre,
+            @PathVariable("idRol") final Long idRol, @PathVariable("activo") final char activo)
+            throws Exception {
+        System.out.println("POST:" + id);
+
+        Estudiantes estcreate = new Estudiantes();
+        estcreate = estudianteService.crearEstudiantes(id, nombre, String.valueOf(idRol), activo);
+
+        System.out.println("Ver cretate2: " + estcreate);
+
+        return ResponseEntity.ok("Usuario creado");
+
+    }
+
+    @RequestMapping(value = "/update/{id}/{nombre}/{idRol}/{activo}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> saveUserUpdate(@PathVariable("id") final Long id,
+            @PathVariable("nombre") final String nombre,
+            @PathVariable("idRol") final Long idRol, @PathVariable("activo") final char activo)
+            throws Exception {
+        System.out.println("PUT:" + id);
+        Estudiantes estcreate = new Estudiantes();
+        estcreate = estudianteService.updateEstudiantes(id, nombre, String.valueOf(idRol), activo);
+        return ResponseEntity.ok("Usuario creado");
+
     }
 
 }
